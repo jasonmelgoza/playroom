114594250a8ac037eeb08fed2166fe7c59881f49
Jason Melgoza <jasonmelgoza@gmail.com>
2020-05-11 10:01:29 -0700

    Added storybook and component example

---
+++ .storybook/main.js
@@ -0,0 +1,9 @@
+module.exports = {
+  stories: ['../src/**/*.stories.js'],
+  addons: [
+    '@storybook/preset-create-react-app',
+    '@storybook/addon-actions',
+    '@storybook/addon-links',
+  ],
+  webpackFinal: (config) => console.dir(config, { depth: null }) || config,
+};
--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,3 +1,16 @@
-import { Button } from 'semantic-ui-react';
+import React from "react";
+import { Button as StyledButton } from 'semantic-ui-react';
+
+import styles from './Button.module.scss';
+
+import "./Button.scss";
+
+function Button(props) {
+  return (
+    <div className={styles.Button}>
+      <StyledButton className="button is-danger is-outlined container">{props.children}</StyledButton>
+    </div>
+  );
+}
 
 export default Button;
